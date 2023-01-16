d1543d05596744f4f84fa6eca56b4f132bd975b9
borossandor27 <boros.sandor@dszcberegszaszi.hu>
2023-01-16 21:00:35 +0100

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppAsyncSzovegolvaso
+{
+    class Program
+    {
+        /*
+         * Néhány aszinkron metódusokat tartalmazó API: 
+         * HttpClient, 
+         * SyndicationClient, 
+         * StorageFile, 
+         * StreamWriter, 
+         * StreamReader, 
+         * XmlReader,
+         * MediaCapture, 
+         * BitmapEncoder, 
+         * BitmapDecoder 
+         * stb.
+         * */
+       
+    static void Main()
+        {
+            Task task = new Task(CallMethod);
+            task.Start();
+            task.Wait();
+            Console.WriteLine("Program vége!");
+            Console.ReadLine();
+        }
+
+        static async void CallMethod()
+        {
+            string filePath = "piszkos_fred_kozbelep.html";
+            Task<int> task = ReadFile(filePath);
+
+            Console.WriteLine(" Other Work 1");
+            Console.WriteLine(" Other Work 2");
+            Console.WriteLine(" Other Work 3");
+
+            int length = await task;
+            Console.WriteLine(" Total length: " + length);
+
+            Console.WriteLine(" After work 1");
+            Console.WriteLine(" After work 2");
+        }
+
+        static async Task<int> ReadFile(string file)
+        {
+            int length = 0;
+
+            Console.WriteLine(" File reading is stating");
+            using (StreamReader reader = new StreamReader(file))
+            {
+                // Reads all characters from the current position to the end of the stream asynchronously
+                // and returns them as one string.
+                string s = await reader.ReadToEndAsync();
+
+                length = s.Length;
+            }
+            Console.WriteLine(" File reading is completed");
+            return length;
+        }
+    }
+}
---
+++ ConsoleAppAsyncLinking/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppAsyncLinking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            callMethod();
+            Console.WriteLine("program vége!");
+            Console.ReadKey();
+        }
+
+        public static async void callMethod()
+        {
+            Task<int> task = Method1();
+            Method2();
+            int count = await task; //-- megvárja, amíg befejeződik a Method1()
+            Method3(count);
+        }
+
+        public static async Task<int> Method1()
+        {
+            int count = 0;
+            await Task.Run(() =>
+            {
+                for (int i = 0; i < 20; i++)
+                {
+                    Console.WriteLine(" Method 1");
+                    count += 1;
+                }
+            });
+            return count;
+        }
+
+        public static void Method2()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(" Method 2");
+            }
+        }
+
+        public static void Method3(int count)
+        {
+            Console.WriteLine("Total count is " + count);
+        }
+    }
+}
